fd5f3bc0627f07b9fff5e4c14eda8541bd84305b
Axel Arroyo <axel.arroyo@sansano.usm.cl>
2022-04-06 00:20:27 -0400

    products-api

--- PaperValdivia/usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from usuarios.views import *
-
-urlpatterns = [
-    path("perfil", perfil),
-    path("registro", registro),
-    path("login", login),
-    path("logout", logout),
-]
--- README.md
+++
@@ -1,5 +0,0 @@
-## Implementaciones restantes
-- Características de un producto
-- Editar productos
-- Eliminar productos
-- Carrito de compras y derivados (compra, historial de compras, etc.)
